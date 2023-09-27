28ca624680c042746ede29cec561be9ac5c3a1fc
shafiqswat <shafiqswat02@gmail.com>
2023-09-27 03:26:47 -0700

    9/27/2023

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 /** @format */
-
+var change = document.querySelector(".discover");
 var col = document.querySelectorAll("#changer");
 window.addEventListener("scroll", function () {
   if (window.scrollY > 100) {
     // Loop through each element with the "news" class and set the background color
     for (var i = 0; i < col.length; i++) {
-      col[i].style.backgroundColor = "white";
+      col[i].style.backgroundColor = "#F5F5F5";
     }
   } else {
     // Reset the background color to transparent
     for (var i = 0; i < col.length; i++) {
       col[i].style.backgroundColor = "transparent";
     }
+  }
+});
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 100) {
+    change.style.color = "black";
+  } else {
+    change.style.color = "white";
   }
 });
